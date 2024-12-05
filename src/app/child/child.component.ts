58e32d95da5712db72b3475c9050b8e244f6d9ba
Saideepak1508 <saigupta0815@gmail.com>
2024-12-05 09:40:35 +0530

    textarea

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -14,4 +14,18 @@ export class ChildComponent {
   send(){
     this.bEvent.emit(10);
   }
+  @Input() max: number = 50;  // Set the max character limit
+  text: string = '';
+
+            // Stores the text entered by the user
+//  @Output() input:EventEmitter<number> = new EventEmitter();
+
+  onInput() {
+    if (this.text.length > this.max) {
+      this.text = this.text.substring(0, this.max);  // Trim the text if it exceeds the max limit
+    }
+  }
 }
+
+
+
